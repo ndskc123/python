a031d998b30e0bc6bab653b68436b3976e2aa2ef
yangyi <yy15330227110@163.com>
2020-11-04 08:21:56 +0800

    a

--- test_yangyi/test_002.py
+++ test_yangyi/test_002.py
@@ -1,5 +1,28 @@
-list1=['','a']
-for i in list1:
-    if i=='':
-        list1.remove('')
-print(list1)
+from appium import webdriver
+import time
+import unittest
+
+class Test_appium(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        phone={
+            'deviceName':'Redmi_5_Plus',
+            'platformName':'Android',
+            'platformVersion':'8.1',
+            'automation':'appium',
+            'appPackage':'com.baidu.searchbox',
+            'appActivity':'com.baidu.searchbox.SplashActivity',
+            'noReset':'True'
+
+        }
+        cls.driver=webdriver.Remote('http://127.0.0.1:4723/wd/hub',phone)
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(2)
+        cls.driver.quit()
+    def test_a(self):
+        time.sleep(2)
+        
+
+if __name__ == '__main__':
+    unittest.main()
